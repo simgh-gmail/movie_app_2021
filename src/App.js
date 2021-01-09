6d9209bdf86cdc49a4d44d9ff93c63f1f21d2273
unknown <tlarbgjs01@gmail.com>
2021-01-10 02:17:00 +0900

    2.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Food(props){
+  console.log(props);
+  return <h2>I like potato.</h2>
+}
 
 function App() {
-  return (
+   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world</h1>
+      <h2>asdf</h2>
+      <Food 
+      fav="chicken"
+      />
     </div>
   );
 }
