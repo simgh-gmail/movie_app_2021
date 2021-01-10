062332164e38d27ef15c8691a47f80852ab18271
SimGH-gmail <tlarbgjs01@gmail.com>
2021-01-11 01:36:50 +0900

    Component Example!!

--- src/App.js
+++ src/App.js
@@ -1,66 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Food({fav}){
-  return <h2>I like {fav}.</h2>
-}
-
-function PropsFood(props){
-  return <h2>I like {props.fav}.</h2>
-}
-
-function WhoAreYou({name, img, rating}){
-  return(
-    <div>
-      <h2>I'm {name}</h2> {rating}/999
-      <img src={img} alt={name}/>
-    </div>
-  )
-}
-
-function renderThing(feature){
-  return(
-    <div>
-      <h2>I'm {feature.name}</h2>
-      <img src={feature.image} alt={feature.name}/>
-    </div>
-  )
-  /*return(
-    <WhoAreYou key={feature.id} name={feature.name} img={feature.image}/>
-  )*/
-}
-
-WhoAreYou.propTypes = {
-  name: PropTypes.string.isRequired,
-  img: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-}
-
-const livingThings =[
-  {
-    id: 1,
-    name: "Grace",
-    image: "./grace.jpg",
-    rating: 123
-  },
-  {
-    id: 2,
-    name: "Manse",
-    image: "./manse.jpg",
-    rating: 456
+class App extends React.Component{
+  //React.Component class doesn't have return but have render and state
+  //state is object ==> put datas that we want to change
+  //DO NOT CHANGE STATE DIRECTLY because react doesn't refresh the render
+  //component is where we put our data.
+  //Only function React component uses is render function
+  constructor(props){   //constructor가 render보다 먼저 호출됨. 왜냐면 이건 js기 때문.
+    super(props);
+    console.log("hi");
   }
-]
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world</h1>
-      <Food fav="chicken"/>
-      <PropsFood fav="삼겹살"/>
-      {livingThings.map(thing=><WhoAreYou key={thing.id} name={thing.name} img={thing.image} rating={thing.rating}/>)}
-      {/*{livingThings.map(renderThing)}*/}
-    </div>
-  );
+  state = {
+    count: 0
+  };
+  add = () => {
+    // this.state.count+=1;   Example of changing state directly.
+    // but that doens't rerender. So we use setState!!!!!! setState "re"render.
+    this.setState({count: this.state.count+1}) //But this isn't cool. since this.state form isn't not cool.
+  };
+  minus = () => {
+    this.setState(current=>({count: current.count - 1}))  //it doesn't depend on external state.
+  };
+  componentDidMount(){    //component의 호출 전후 이런 과정을 제어
+    //이런 것들을 component life cycle이라 함.
+    console.log("component rendered");
+  }
+  componentDidUpdate(){
+    console.log("I just updated");
+  }
+  componentWillUnmount(){
+    console.log("Goodbye, unmount.");
+  }
+  render(){
+    console.log("I'm rendering");
+    return <div>
+      <h1>The number is: {this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
+      </div>
+  }
 }
 
 export default App;
