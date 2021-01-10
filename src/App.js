7ddf499bdd9fde05bc6c7fba0af38f5a145da223
unknown <tlarbgjs01@gmail.com>
2021-01-10 15:49:58 +0900

    added image and example of using jsx

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import React from 'react';
 
-function Food(props){
-  console.log(props);
-  return <h2>I like potato.</h2>
+function Food({fav}){
+  return <h2>I like {fav}.</h2>
 }
 
+function PropsFood(props){
+  return <h2>I like {props.fav}.</h2>
+}
+
+function WhoAreYou({name, img}){
+  return(
+    <div>
+      <h2>I'm {name}</h2>
+      <img src={img} alt={name}/>
+    </div>
+  )
+}
+
+const foodILike = ["apple", "you"];
+
+const livingThings =[
+  {
+    name: "Grace",
+    image: "./grace.jpg"
+  },
+  {
+    name: "Manse",
+    image: "./manse.jpg"
+  }
+]
+
 function App() {
-   return (
+  return (
     <div className="App">
       <h1>Hello world</h1>
-      <h2>asdf</h2>
-      <Food 
-      fav="chicken"
-      />
+      <Food fav="chicken"/>
+      <PropsFood fav="삼겹살"/>
+      {foodILike.map(function(foodName){
+        return <Food fav = {foodName}/>
+      })}
+      {foodILike.map(foodName => <Food fav={foodName}/>)}
+      {livingThings.map(thing => <WhoAreYou name={thing.name} img={thing.image}/>)}
     </div>
   );
 }
