16531a4eaf494a138e3e2db12ea956c2c162c077
SimGH-gmail <tlarbgjs01@gmail.com>
2021-01-10 20:36:32 +0900

    PROPS Example

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function Food({fav}){
   return <h2>I like {fav}.</h2>
@@ -8,25 +9,45 @@ function PropsFood(props){
   return <h2>I like {props.fav}.</h2>
 }
 
-function WhoAreYou({name, img}){
+function WhoAreYou({name, img, rating}){
   return(
     <div>
-      <h2>I'm {name}</h2>
+      <h2>I'm {name}</h2> {rating}/999
       <img src={img} alt={name}/>
     </div>
   )
 }
 
-const foodILike = ["apple", "you"];
+function renderThing(feature){
+  return(
+    <div>
+      <h2>I'm {feature.name}</h2>
+      <img src={feature.image} alt={feature.name}/>
+    </div>
+  )
+  /*return(
+    <WhoAreYou key={feature.id} name={feature.name} img={feature.image}/>
+  )*/
+}
+
+WhoAreYou.propTypes = {
+  name: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+}
 
 const livingThings =[
   {
+    id: 1,
     name: "Grace",
-    image: "./grace.jpg"
+    image: "./grace.jpg",
+    rating: 123
   },
   {
+    id: 2,
     name: "Manse",
-    image: "./manse.jpg"
+    image: "./manse.jpg",
+    rating: 456
   }
 ]
 
@@ -36,11 +57,8 @@ function App() {
       <h1>Hello world</h1>
       <Food fav="chicken"/>
       <PropsFood fav="삼겹살"/>
-      {foodILike.map(function(foodName){
-        return <Food fav = {foodName}/>
-      })}
-      {foodILike.map(foodName => <Food fav={foodName}/>)}
-      {livingThings.map(thing => <WhoAreYou name={thing.name} img={thing.image}/>)}
+      {livingThings.map(thing=><WhoAreYou key={thing.id} name={thing.name} img={thing.image} rating={thing.rating}/>)}
+      {/*{livingThings.map(renderThing)}*/}
     </div>
   );
 }
