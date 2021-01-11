a3b58fbb10b7896132f5e00d86e41807d5b6c3c8
SimGH-gmail <tlarbgjs01@gmail.com>
2021-01-11 10:39:44 +0900

    example of using componentDidMount

--- src/App.js
+++ src/App.js
@@ -7,38 +7,21 @@ class App extends React.Component{
   //DO NOT CHANGE STATE DIRECTLY because react doesn't refresh the render
   //component is where we put our data.
   //Only function React component uses is render function
-  constructor(props){   //constructor가 render보다 먼저 호출됨. 왜냐면 이건 js기 때문.
-    super(props);
-    console.log("hi");
-  }
   state = {
-    count: 0
+    isLoading: true
   };
-  add = () => {
-    // this.state.count+=1;   Example of changing state directly.
-    // but that doens't rerender. So we use setState!!!!!! setState "re"render.
-    this.setState({count: this.state.count+1}) //But this isn't cool. since this.state form isn't not cool.
-  };
-  minus = () => {
-    this.setState(current=>({count: current.count - 1}))  //it doesn't depend on external state.
-  };
-  componentDidMount(){    //component의 호출 전후 이런 과정을 제어
-    //이런 것들을 component life cycle이라 함.
-    console.log("component rendered");
-  }
-  componentDidUpdate(){
-    console.log("I just updated");
-  }
-  componentWillUnmount(){
-    console.log("Goodbye, unmount.");
+  componentDidMount(){
+    setTimeout(()=>{
+    this.setState({isLoading: false})},2000);
   }
   render(){
-    console.log("I'm rendering");
-    return <div>
-      <h1>The number is: {this.state.count}</h1>
-      <button onClick={this.add}>Add</button>
-      <button onClick={this.minus}>Minus</button>
+    const {isLoading}=this.state;     //ES6 문법 
+    return(
+      <div>
+        {this.state.isLoading ? "Loading" :"We are ready"}
+        {isLoading ? "Loading..." :"We are ready"}
       </div>
+    )
   }
 }
 
